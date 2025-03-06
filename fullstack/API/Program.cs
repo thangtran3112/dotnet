86e1d1b50f53fd4604c24da74f692dc12436d5aa
Thang Tran <37521149+thangtran3112@users.noreply.github.com>
2025-03-05 21:31:12 -0800

    add Cors

--- fullstack/API/Program.cs
+++ fullstack/API/Program.cs
@@ -25,6 +25,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
