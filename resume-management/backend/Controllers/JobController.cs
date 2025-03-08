b5e40340096d67ccb087ebe9b2dc5ed5e6223720
Thang Tran <37521149+thangtran3112@users.noreply.github.com>
2025-03-07 22:29:43 -0800

    get jobs

--- resume-management/backend/Controllers/JobController.cs
+++ resume-management/backend/Controllers/JobController.cs
@@ -7,6 +7,7 @@ using backend.Core.Context;
 using backend.Core.Dtos.Job;
 using backend.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
@@ -32,6 +33,15 @@ namespace backend.Controllers
             await _context.Jobs.AddAsync(newJob);
             await _context.SaveChangesAsync();
             return Ok("Job created successfully");
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<ActionResult<IEnumerable<JobGetDto>>> GetJobs()
+        {
+            var jobs = await _context.Jobs.Include(job => job.Company).ToListAsync();
+            var convertedJobs = _mapper.Map<IEnumerable<JobGetDto>>(jobs);
+            return Ok(convertedJobs);
         }
     }
 }
